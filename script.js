93a5dd631a2c12f91791e16f651bc2817500871c
VANDANA K <vandanakbng@gmail.com>
2026-03-10 09:47:21 +0000

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("click", function(e){
+
+const target = this.getAttribute("href");
+
+if(target.startsWith("#")){
+
+e.preventDefault();
+
+document.querySelector(target).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+});
+
+});
